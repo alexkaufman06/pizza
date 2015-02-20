9e05dbbb615b379d53c96616301bdcfc975d78d2
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-20 10:25:49 -0800

    Add specs for pizza size and pepperoni toppings

--- specs/specs.js
+++ specs/specs.js
@@ -5,16 +5,35 @@ describe("Pizza", function() {
       expect(testPizza.price()).to.equal(5);
     });
 
-    it("returns the price of a 12 inch pizza", function() {
+    it("returns the price of a 8-12 inch pizza", function() {
       var testPizza = Object.create(Pizza);
       testPizza.size = 12;
       expect(testPizza.price()).to.equal(14);
     });
 
-    it("returns the price of a 14 inch pizza", function() {
+    it("returns the price of a 13-14 inch pizza", function() {
       var testPizza = Object.create(Pizza);
       testPizza.size = 14;
       expect(testPizza.price()).to.equal(16);
+    });
+
+    it("returns the price of a 15-16 inch pizza", function() {
+      var testPizza = Object.create(Pizza);
+      testPizza.size = 16;
+      expect(testPizza.price()).to.equal(18);
+    });
+
+    it("returns the price of a 17-18 inch pizza", function() {
+      var testPizza = Object.create(Pizza);
+      testPizza.size = 18;
+      expect(testPizza.price()).to.equal(20);
+    });
+
+    it("returns the additional price for ordering a pepperoni pizza", function() {
+      var testPizza = Object.create(Pizza);
+      testPizza.size = 18;
+      testPizza.type = "pepperoni";
+      expect(testPizza.price()).to.equal(22);
     });
   });
 });
