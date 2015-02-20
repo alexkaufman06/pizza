a2e3f6a92b885487b6fa6938345412c407cad650
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-20 09:51:57 -0800

    Add spec for a 14 inch pizza

--- specs/specs.js
+++ specs/specs.js
@@ -2,13 +2,19 @@ describe("Pizza", function() {
   describe("price()", function() {
     it("returns the base price of cooking a pizza", function() {
       var testPizza = Object.create(Pizza);
-      expect(testPizza.price()).to.equal(1);
+      expect(testPizza.price()).to.equal(5);
     });
 
-    it("returns the price of a 5 inch pizza", function() {
+    it("returns the price of a 12 inch pizza", function() {
       var testPizza = Object.create(Pizza);
-      testPizza.size = 5;
-      expect(testPizza.price()).to.equal(3);
+      testPizza.size = 12;
+      expect(testPizza.price()).to.equal(14);
+    });
+
+    it("returns the price of a 14 inch pizza", function() {
+      var testPizza = Object.create(Pizza);
+      testPizza.size = 14;
+      expect(testPizza.price()).to.equal(16);
     });
   });
 });
