12f1f466f5c078ce107690a555046bc1689dfc83
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-20 10:26:16 -0800

    Add scripts for pizza size and pepperoni toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
 var Pizza = {
   basePrice: 5,
+  toppings: ["pepperoni", "chicken"],
   price: function() {
-    if (this.size === 12) {
+    if (this.size <= 12) {
       this.basePrice += 9;
-    } else if (this.size === 14) {
+    } else if (this.size <= 14 && this.size > 12) {
       this.basePrice += 11;
+    } else if (this.size <= 16 && this.size > 14) {
+      this.basePrice += 13;
+    } else if (this.size <= 18 && this.size > 16) {
+      this.basePrice += 15;
+    }
+
+    if (this.toppings.indexOf(this.type) !== -1) {
+      this.basePrice += 2;
     }
   return this.basePrice;
   }
