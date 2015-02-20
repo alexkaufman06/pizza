bd89e047e8a084037f41a13b6928f8901cada6d8
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-20 10:47:21 -0800

    Add scripts for slices method

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var Pizza = {
   basePrice: 5,
+  baseSlices: 8,
   toppings: ["pepperoni", "chicken", "sausage"],
   price: function() {
     if (this.size <= 12) {
@@ -16,5 +17,22 @@ var Pizza = {
       this.basePrice += 2;
     }
   return this.basePrice;
+  },
+
+  slices: function() {
+    if (this.size <= 12) {
+      this.baseSlices;
+    } else if (this.size <= 14 && this.size > 12) {
+      this.baseSlices += 1;
+    } else if (this.size <= 16 && this.size > 14) {
+      this.baseSlices += 2;
+    } else if (this.size <= 18 && this.size > 16) {
+      this.baseSlices += 3;
+    }
+
+    if (this.toppings.indexOf(this.type) !== -1) {
+      this.baseSlices -= 1;
+    }
+  return this.baseSlices;
   }
 };
