fed24906a466a512fe1699c4f1023ef00f50e8f1
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-20 09:22:08 -0800

    Add spec for price function

---
+++ specs/specs.js
@@ -0,0 +1,7 @@
+describe("Pizza", function() {
+  describe("price()", function() {
+    it("returns the initial price of cooking a pizza")
+      var testPizza = Object.create(Pizza);
+      expect(textPizza.price()).to.equal(1);
+  });
+});
