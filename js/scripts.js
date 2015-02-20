f70b5fe1508a30367062c54aca0a91d7f16abdaf
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-20 09:52:25 -0800

    Add script for a 14 inch pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 var Pizza = {
-  basePrice: 1,
+  basePrice: 5,
   price: function() {
-    if (this.size === 5) {
-      this.basePrice += 2;
+    if (this.size === 12) {
+      this.basePrice += 9;
+    } else if (this.size === 14) {
+      this.basePrice += 11;
     }
   return this.basePrice;
   }
